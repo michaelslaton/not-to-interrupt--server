7f9deecf9ee267ef74a978cb44c3db5d1c28594f
RenShadecrest <michaelaaronphoto@gmail.com>
2025-05-22 18:38:01 -0700

    added more broken down functions to allow for singular events such as creating a room and having more control over what intervals are sent to the client

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,64 @@ const io = new Server(httpServer, {
     origin: 'http://localhost:5173',
   },
 });
+const activeIntervals = new Map<string, NodeJS.Timeout>();
 
-let crudData: any[] = [];
+let roomList: any[] = [];
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
+  console.log(`Client connected`);
+  
+  socket.on('getRoomList', () => {
+    if (activeIntervals.has(socket.id)) {
+      clearInterval(activeIntervals.get(socket.id));
+      activeIntervals.delete(socket.id);
+    };
+    
+    const interval = setInterval(() => {
+      socket.emit('getRoomList', roomList);
+    }, 1000);
+    
+    activeIntervals.set(socket.id, interval);
+  });
 
-  socket.on('create', (data)=>{
-    crudData.push(data);
-    socket.emit('crudData', crudData);
-  })
+  socket.on('createRoom', (roomData)=>{
+    roomList.push(roomData);
+    
+    if (activeIntervals.has(socket.id)) {
+      clearInterval(activeIntervals.get(socket.id));
+      activeIntervals.delete(socket.id);
+    };
 
-  setInterval(() => {
-    socket.emit('crudData', crudData);
-  }, 1000);
+    const interval = setInterval(() => {
+      const data = roomList.find((room)=> room.roomId === roomData.roomId);
+      socket.emit('roomData', data);
+    }, 1000);
+
+    activeIntervals.set(socket.id, interval);
+  });
+
+  socket.on('enterRoom', (roomId) => {
+    if (activeIntervals.has(socket.id)) {
+      clearInterval(activeIntervals.get(socket.id));
+      activeIntervals.delete(socket.id);
+    };
+
+    const interval = setInterval(() => {
+      const data = `The data is ${roomId}`
+      socket.emit('roomData', data);
+    }, 1000);
+
+    activeIntervals.set(socket.id, interval);
+  });
+
+  socket.on('disconnect', () => {
+    const interval = activeIntervals.get(socket.id);
+    if (interval) {
+      clearInterval(interval);
+      activeIntervals.delete(socket.id);
+    }
+    console.log(`Client disconnected: ${socket.id}`);
+  });
 });
 
 const PORT = process.env.PORT || 3000;
